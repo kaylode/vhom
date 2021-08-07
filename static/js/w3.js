2a3a16f31e3bb550d76c135385681306db72cc04
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-08-08 02:05:22 +0700

    add static files

---
+++ static/js/w3.js
@@ -0,0 +1,16 @@
+function onClick(e) {
+  console.log(this.getLatLng());
+}
+
+function w3_open() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+  }
+  
+function w3_close() {
+document.getElementById("main").style.marginLeft = "0%";
+document.getElementById("mySidebar").style.display = "none";
+document.getElementById("openNav").style.display = "inline-block";
+}
