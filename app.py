15796027974351dfe30a66a483cd1a18901b7096
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-08-07 11:21:39 +0700

    add request data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import folium
-from modules import get_map
+from modules import get_map, request_data
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def index():
     folium_map = get_map()
     return folium_map._repr_html_()
 
+@app.route('/data')
+def request():
+    data = request_data('http://192.168.100.16:4000/api/request')
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
