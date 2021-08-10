77c312c97a5490efd2e096a942ddb5268b1157ca
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-08-10 21:05:20 +0700

    add onclick marker data

--- modules/custom.py
+++ modules/custom.py
@@ -4,18 +4,20 @@ from jinja2 import Template
 
 class OnClickMarker(folium.Marker):
     def __init__(self, location=None, popup=None, tooltip=None, icon=None, 
-                 draggable=False, on_click=None, **kwargs):
+                 draggable=False, on_click=None, on_click_data=None, **kwargs):
         super().__init__(location=location, popup=popup, tooltip=tooltip, icon=icon, draggable=draggable, **kwargs)
-        self.on_click = on_click
-
-        self._template = Template(u"""
-        {% macro script(this, kwargs) %}
-            var {{ this.get_name() }} = L.marker(
-                {{ this.location|tojson }},
-                {{ this.options|tojson }}
-            ).addTo({{ this._parent.get_name() }}).on('click',{{ this.on_click }});
-        {% endmacro %}
-        """)
+        self.on_click = on_click if on_click is not None else None
+        self.on_click_data = on_click_data if on_click_data is not None else "{ }"
+        
+        if self.on_click is not None:
+            self._template = Template(u"""
+            {% macro script(this, kwargs) %}
+                var {{ this.get_name() }} = L.marker(
+                    {{ this.location|tojson }},
+                    {{ this.options|tojson }}
+                ).addTo({{ this._parent.get_name() }}).on('click', function(e){ {{ this.on_click }} (e, {{ this.on_click_data }}) });
+            {% endmacro %}
+            """)
     
 class CustomJavaScript(folium.JavascriptLink):
     def __init__(self, url, type=None, download=False):
