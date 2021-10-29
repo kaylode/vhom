03e45a6821666d3ca41fca3dde9f27eb3754c0d0
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-10-29 21:44:02 +0700

    add interactive shell scripts for running docker

--- run_console.sh
+++ run_console.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname $(realpath $0))
+USERNAME=guest
 
 mkdir -p $SCRIPT_DIR/.vscode-server
 
 docker run --rm -it --name web-server \
 	-p 5000:5000 \
+	--volume $SCRIPT_DIR/src:/home/${USERNAME}/src \
+	--volume $SCRIPT_DIR/data:/home/${USERNAME}/data \
+	--workdir /home/${USERNAME}/ \
 	$1 
-	# --volume $SCRIPT_DIR/workspace:/home/aidata/workspace \
-	# --workdir /home/aidata/workspace \
-	# --volume $SCRIPT_DIR/.vscode-server:/home/aidata/.vscode-server \
-	# --gpus all --shm-size 2g \
